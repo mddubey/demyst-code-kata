85ba9386668a7240d312e268efc77b7ca367f55d
mddubey <mddubey409@gmail.com>
2024-04-20 13:29:26 +0530

    Binary script and command line arguments

--- src/todo-dashboard.js
+++ src/todo-dashboard.js
@@ -1,6 +1,8 @@
 const { todoService } = require("./todo-service");
 
+const limit = parseInt(process.argv[0]) || 20
 console.log("todo-dashboard");
-todoService.fetchToDos(20)
+console.log(`Fetching top ${limit} matching todos...`);
+todoService.fetchToDos(limit)
     .then((todos) => console.table(todos))
     .catch((error) => console.log("Error while fetching TODOs"));
