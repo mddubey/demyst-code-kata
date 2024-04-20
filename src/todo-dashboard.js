8e9d07cf1d9231e340abff103c08c826fe229f2f
mddubey <mddubey409@gmail.com>
2024-04-20 13:32:51 +0530

    Fix process arguments

--- src/todo-dashboard.js
+++ src/todo-dashboard.js
@@ -1,6 +1,7 @@
 const { todoService } = require("./todo-service");
 
-const limit = parseInt(process.argv[0]) || 20
+const limit = parseInt(process.argv[2]) || 20
+
 console.log("todo-dashboard");
 console.log(`Fetching top ${limit} matching todos...`);
 todoService.fetchToDos(limit)
